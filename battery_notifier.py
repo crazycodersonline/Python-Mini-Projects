8b963b012888cf181281a79ae8c6e878a0a9a8b4
Nitin Madas <80050646+nitinmadas@users.noreply.github.com>
2021-08-17 19:02:45 +0530

    deleted battery notifier

--- battery_notifier.py
+++
@@ -1,7 +0,0 @@
-from pynotifier import Notification
-import psutil
-
-battery = psutil.sensors_battery()
-percent = battery.percent
-
-Notification("Battery Percentage" , str(percent) + "% percentage remaining" , duration=10).send()
